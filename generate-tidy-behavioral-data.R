65b069a91adc6427c311e77c9b897419d7caf8ff
Josh de Leeuw <josh.deleeuw@gmail.com>
2019-03-06 15:23:19 -0500

    update to latest code

--- generate-tidy-behavioral-data.R
+++ generate-tidy-behavioral-data.R
@@ -1,15 +1,36 @@
 library(readr)
+library(stringr)
 
 all.behavioral.files <- dir('data/raw/behavioral/')
 
-all.behavioral.data <- NA
+raw.behavioral.data <- NA
+#f <- all.behavioral.files[34]
 for(f in all.behavioral.files){
-  this.p.data <- read_csv(paste0('data/raw/behavioral/',f))
+  subject_id <- f %>% stringr::str_sub(start=13,end=14)
+  this.p.data <- read_csv(paste0('data/raw/behavioral/',f),locale = locale(encoding = 'UTF-8'))
+  this.p.data$participant_id <- subject_id
   if(is.na(all.behavioral.data)){
-    all.behavioral.data <- this.p.data
+    raw.behavioral.data <- this.p.data
   } else {
-    all.behavioral.data <- rbind(all.behavioral.data, this.p.data)
+    raw.behavioral.data <- rbind(raw.behavioral.data, this.p.data)
   }
 }
 
-write_csv(all.behavioral.data, 'data/tidy/behavioral-data-tidy.csv')
+# basic check on data length
+
+n.per.subject <- raw.behavioral.data %>% group_by(participant_id) %>% summarize(N=n())
+
+# clearly we have some data corruption issues in some files. can we identify trials by
+# looking specifically for the critical test trials?
+
+test.behavioral.data <- raw.behavioral.data %>% 
+  filter(phase=='test', correct %in% c("true", "false"), distractor_type %in% c("bas", "neu", "neg"), target_lag %in% c(2,8)) %>% 
+  select(participant_id, trial_index, distractor_type, target_lag, target_orientation, distractor_position, correct, rt, button_pressed, phase)
+
+# we should have 600 trials per subject
+
+n.test.per.subject <- test.behavioral.data %>% group_by(participant_id) %>% summarize(N=n())
+
+write_csv(test.behavioral.data, 'data/tidy/behavioral-data-tidy.csv')
+
+#guess_encoding('data/raw/behavioral/participant_39_behavioral.csv')
--- generate-tidy-eeg-data.R
+++ generate-tidy-eeg-data.R
@@ -15,22 +15,33 @@ library(dplyr)
 all.files <- dir('data/raw/eeg')
 
 all.data <- NA
+all.correct.data <- NA
 for(f in all.files){
   subject <- f %>% stringr::str_sub(start=14,end=15)
-  lag.condition <- f %>% stringr::str_extract("(Lag)[2,8]+")
-  distractor.condition <- f %>% stringr::str_extract("(?<=Lag[2,8])\\w+")
   file.data <- read_table2(paste0('data/raw/eeg/',f), col_names = as.character(1:129))
   file.data$t <- seq(from=-200, to=1195, by=5)
   file.data.tidy <- file.data %>% gather(key="electrode", value=voltage, 1:129)
   file.data.tidy$subject <- subject
-  file.data.tidy$lag.condition <- lag.condition
-  file.data.tidy$distractor.condition <- distractor.condition
-  if(is.na(all.data)){
-    all.data <- file.data.tidy
+  if(any(f %>% str_detect(c("Correct","Incorrect")))){
+    correct.condition <- f %>% stringr::str_extract("(?<=L2N)\\w+")
+    file.data.tidy$correct.condition <- correct.condition
+    if(is.na(all.correct.data)){
+      all.correct.data <- file.data.tidy
+    } else {
+      all.correct.data <- rbind(all.correct.data, file.data.tidy)
+    }
   } else {
-    all.data <- rbind(all.data, file.data.tidy)
+    lag.condition <- f %>% stringr::str_extract("(Lag)[2,8]+")
+    distractor.condition <- f %>% stringr::str_extract("(?<=Lag[2,8])\\w+")
+    file.data.tidy$lag.condition <- lag.condition
+    file.data.tidy$distractor.condition <- distractor.condition
+    if(is.na(all.data)){
+      all.data <- file.data.tidy
+    } else {
+      all.data <- rbind(all.data, file.data.tidy)
+    }
   }
 }
 
-
 write_csv(all.data, path="data/tidy/eeg-data-tidy.csv")
+write_csv(all.correct.data, path="data/tidy/eeg-data-correct-incorrect-tidy.csv")
